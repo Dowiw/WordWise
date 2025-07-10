529af845c375e47ea5a6de1eb94c9739eee715ef
Dowiw <monjie12272@gmail.com>
2025-07-10 14:49:17 +0200

    refactor: utils

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -10,7 +10,7 @@ using namespace std;
 volatile int processingFlag = 0;
 static pthread_t loadingThreadId;
 
-// Mutex for thread-safe console output in saveWordThreadFunc
+// mutex for thread-safe console output in saveWordThreadFunc
 extern pthread_mutex_t cout_mutex;
 
 void* loadingAnimationThreadFunc(void* arg) {
@@ -49,23 +49,29 @@ void clearScreen() {
 
 void* saveWordThreadFunc(void* arg) {
 	SaveWordArgs* args = (SaveWordArgs*)arg;
-	auto start = std::chrono::high_resolution_clock::now();
+	auto start = chrono::high_resolution_clock::now();
+
 	pthread_mutex_lock(&cout_mutex);
-	std::cout << "[Thread] Saving stats for word: '" << args->germanWord << "'...\n";
+	cout << "[Thread] Saving stats for word: '" << args->germanWord << "'...\n";
 	pthread_mutex_unlock(&cout_mutex);
-	wordDB.updateWordStats(args->germanWord, args->wasCorrect);
+
+	wordDB.updateWordStats(args->germanWord, args->wasCorrect); // the main update method
 	usleep(300000); // 0.3s for demo
-	auto end = std::chrono::high_resolution_clock::now();
-	double elapsed_ms = std::chrono::duration<double, std::milli>(end - start).count();
+
+	auto end = chrono::high_resolution_clock::now();
+	double elapsed_ms = chrono::duration<double, milli>(end - start).count();
+
 	pthread_mutex_lock(&cout_mutex);
-	std::cout << "[Thread] Done saving: '" << args->germanWord << "' (" << elapsed_ms << " ms)\n";
+	cout << "[Thread] Done saving: '" << args->germanWord << "' (" << elapsed_ms << " ms)\n";
 	pthread_mutex_unlock(&cout_mutex);
+
 	delete args;
 	return nullptr;
 }
 
-void saveWordsInParallel(const std::vector<std::pair<std::string, bool>>& wordResults) {
-	std::vector<pthread_t> threads(wordResults.size());
+void saveWordsInParallel(const vector<pair<string, bool>>& wordResults) {
+	vector<pthread_t> threads(wordResults.size());
+
 	for (size_t i = 0; i < wordResults.size(); ++i) {
 		SaveWordArgs* args = new SaveWordArgs{wordResults[i].first, wordResults[i].second};
 		pthread_create(&threads[i], nullptr, saveWordThreadFunc, args);
@@ -73,5 +79,5 @@ void saveWordsInParallel(const std::vector<std::pair<std::string, bool>>& wordRe
 	for (size_t i = 0; i < threads.size(); ++i) {
 		pthread_join(threads[i], nullptr);
 	}
-	std::cout << "All word stats saved in parallel!\n";
+	cout << "All word stats saved in parallel!\n";
 }
